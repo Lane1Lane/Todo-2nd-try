d6d6d3c02b817d776f03cfb1de248f2f08d35a10
andrejmelnyk <andrejmelnyk@gmail.com>
2019-05-20 12:35:36 +0300

    Added POST /users

--- server/server.js
+++ server/server.js
@@ -93,6 +93,15 @@ app.patch('/todos/:id', (req,res) => {
     }).catch((e) => {res.status(400).send()});
 });
 
+app.post('/users', (req, res) => {
+    var user = new User(_.pick(req.body,['name', 'email', 'password']));
+
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {res.status(400).send(e)})
+});
+
+
 app.listen(port,() => {
     console.log(`Started at port ${port}`);
 });
